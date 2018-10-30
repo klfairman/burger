248890379b3c5e1fecb23a392ebff09036133dce
Kristen Fairman <fairman.kristen@gmail.com>
2018-10-29 20:40:24 -0500

    Final

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Grassfed Kobe Beef Burger",
+0
+),
+(
+"Turkey Burger",
+0
+),
+(
+"Veggie Burger",
+0
+),
+(
+"Bacon Double Cheese Burger",
+0
+),
+(
+"Falafel Burger",
+0
+);
